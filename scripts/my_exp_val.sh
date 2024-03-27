b5b6acca246f8c69bb704029e1720be53eb3dbbb
zhangyuzhan <511586683@qq.com>
2024-03-27 20:22:37 +0800

    add test

--- scripts/my_exp_val.sh
+++ scripts/my_exp_val.sh
@@ -8,5 +8,7 @@ sudo python3 run.py config_test_v128.yml my_ycsb_log
 echo "128 done"
 sudo python3 run.py config_test_v256.yml my_ycsb_log
 echo "256 done"
-sudo python3 run.py config_test_v980.yml my_ycsb_log
-echo "980 done"
+sudo python3 run.py config_test_v512.yml my_ycsb_log
+echo "512 done"
+# sudo python3 run.py config_test_v980.yml my_ycsb_log
+# echo "980 done"
