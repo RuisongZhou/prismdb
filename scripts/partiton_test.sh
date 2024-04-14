7aae671f5f146f6ad1ad3891ab47fff979ea9547
ruisongzhou <811437508@qq.com>
2024-04-14 11:52:04 +0800

    fix bugs

--- scripts/partiton_test.sh
+++ scripts/partiton_test.sh
@@ -1,13 +1,12 @@
-sudo python3 run.py config_test_partition_1.yml my_ycsb_log
-echo "1 done"
-sudo python3 run.py config_test_partition_2.yml my_ycsb_log
-echo "2 done"
-sudo python3 run.py config_test_partition_4.yml my_ycsb_log
-echo "4 done"
-sudo python3 run.py config_test_partition_8.yml my_ycsb_log
-echo "8 done"
 sudo python3 run.py config_test_partition_16.yml my_ycsb_log
 echo "16 done"
-
+sudo python3 run.py config_test_partition_8.yml my_ycsb_log
+echo "8 done"
+sudo python3 run.py config_test_partition_4.yml my_ycsb_log
+echo "4 done"
+sudo python3 run.py config_test_partition_2.yml my_ycsb_log
+echo "2 done"
+sudo python3 run.py config_test_partition_1.yml my_ycsb_log
+echo "1 done"
 # sudo python3 run.py config_test_v980.yml my_ycsb_log
 # echo "980 done"
