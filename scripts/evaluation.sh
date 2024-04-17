8740fda28717fe64530eb8837cb65bb5d89fe520
zhangyuzhan <511586683@qq.com>
2024-04-17 20:32:29 +0800

    add bench

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -1,79 +1,79 @@
 echo "Running Experiment 1"
 sudo python3 run.py config_exp1.yml my_ycsb_log_exp1
-sed -i 's/ycsba/ycsbb/' config_exp1.yml
+sed -i 's/workload: \[\"ycsba/workload: \[\"ycsbb/' config_exp1.yml
 sudo python3 run.py config_exp1.yml my_ycsb_log_exp1
-sed -i 's/ycsbb/ycsbc/' config_exp1.yml
+sed -i 's/workload: \[\"ycsbb/workload: \[\"ycsbc/' config_exp1.yml
 sudo python3 run.py config_exp1.yml my_ycsb_log_exp1
-sed -i 's/ycsbc/ycsbd/' config_exp1.yml
+sed -i 's/workload: \[\"ycsbc/workload: \[\"ycsbd/' config_exp1.yml
 sudo python3 run.py config_exp1.yml my_ycsb_log_exp1
-sed -i 's/ycsbd/ycsbe/' config_exp1.yml
+sed -i 's/workload: \[\"ycsbd/workload: \[\"ycsbe/' config_exp1.yml
 sudo python3 run.py config_exp1.yml my_ycsb_log_exp1
-sed -i 's/ycsbe/ycsbf/' config_exp1.yml
+sed -i 's/workload: \[\"ycsbe/workload: \[\"ycsbf/' config_exp1.yml
 sudo python3 run.py config_exp1.yml my_ycsb_log_exp1
 
-sed -i 's/ycsbf/ycsba/' config_exp1.yml
+sed -i 's/workload: \[\"ycsbf/workload: \[\"ycsba/' config_exp1.yml
 echo "Experiment 1 done"
 
 echo "Running Experiment 2"
 # 纯写
 sudo python3 run.py config_exp2.yml my_ycsb_log_exp2
-sed -i 's/(0/(0.4/' config_exp2.yml
+sed -i 's/(0 -1)/(0.4 -1)/' config_exp2.yml
 sudo python3 run.py config_exp2.yml my_ycsb_log_exp2
-sed -i 's/(0.4/(0.6/' config_exp2.yml
+sed -i 's/(0.4 -1)/(0.6 -1)/' config_exp2.yml
 sudo python3 run.py config_exp2.yml my_ycsb_log_exp2
-sed -i 's/(0.6/(0.8/' config_exp2.yml
+sed -i 's/(0.6 -1)/(0.8 -1)/' config_exp2.yml
 sudo python3 run.py config_exp2.yml my_ycsb_log_exp2
-sed -i 's/(0.8/(0.99/' config_exp2.yml
+sed -i 's/(0.8 -1)/(0.99 -1)/' config_exp2.yml
 sudo python3 run.py config_exp2.yml my_ycsb_log_exp2
-sed -i 's/(0.99/(1.2/' config_exp2.yml
+sed -i 's/(0.99 -1)/(1.2 -1)/' config_exp2.yml
 sudo python3 run.py config_exp2.yml my_ycsb_log_exp2
-sed -i 's/(1.2/(0/' config_exp2.yml
+sed -i 's/(1.2 -1)/(0 -1)/' config_exp2.yml
 # 纯读
 sed -i 's/read_ratio: \[0\]/read_ratio: \[1\]/' config_exp2.yml
 sudo python3 run.py config_exp2.yml my_ycsb_log_exp2
-sed -i 's/(0/(0.4/' config_exp2.yml
+sed -i 's/(0 -1)/(0.4 -1)/' config_exp2.yml
 sudo python3 run.py config_exp2.yml my_ycsb_log_exp2
-sed -i 's/(0.4/(0.6/' config_exp2.yml
+sed -i 's/(0.4 -1)/(0.6 -1)/' config_exp2.yml
 sudo python3 run.py config_exp2.yml my_ycsb_log_exp2
-sed -i 's/(0.6/(0.8/' config_exp2.yml
+sed -i 's/(0.6 -1)/(0.8 -1)/' config_exp2.yml
 sudo python3 run.py config_exp2.yml my_ycsb_log_exp2
-sed -i 's/(0.8/(0.99/' config_exp2.yml
+sed -i 's/(0.8 -1)/(0.99 -1)/' config_exp2.yml
 sudo python3 run.py config_exp2.yml my_ycsb_log_exp2
-sed -i 's/(0.99/(1.2/' config_exp2.yml
+sed -i 's/(0.99 -1)/(1.2 -1)/' config_exp2.yml
 sudo python3 run.py config_exp2.yml my_ycsb_log_exp2
 
-sed -i 's/(1.2/(0/' config_exp2.yml
+sed -i 's/(1.2 -1)/(0 -1)/' config_exp2.yml
 sed -i 's/read_ratio: \[1\]/read_ratio: \[0\]/' config_exp2.yml
 echo "Experiment 2 done"
 
 echo "Running Experiment 3"
 sudo python3 run.py config_exp3.yml my_ycsb_log_exp3
-sed -i 's/16/32/' config_exp3.yml
+sed -i 's/value_size: \[16\]/value_size: \[32\]/' config_exp3.yml
 sudo python3 run.py config_exp3.yml my_ycsb_log_exp3
-sed -i 's/32/64/' config_exp3.yml
+sed -i 's/value_size: \[32\]/value_size: \[64\]/' config_exp3.yml
 sudo python3 run.py config_exp3.yml my_ycsb_log_exp3
-sed -i 's/64/128/' config_exp3.yml
+sed -i 's/value_size: \[64\]/value_size: \[128\]/' config_exp3.yml
 sudo python3 run.py config_exp3.yml my_ycsb_log_exp3
-sed -i 's/128/256/' config_exp3.yml
+sed -i 's/value_size: \[128\]/value_size: \[256\]/' config_exp3.yml
 sudo python3 run.py config_exp3.yml my_ycsb_log_exp3
-sed -i 's/256/512/' config_exp3.yml
+sed -i 's/value_size: \[256\]/value_size: \[512\]/' config_exp3.yml
 sudo python3 run.py config_exp3.yml my_ycsb_log_exp3
-sed -i 's/512/980/' config_exp3.yml
+sed -i 's/value_size: \[512\]/value_size: \[980\]/' config_exp3.yml
 sudo python3 run.py config_exp3.yml my_ycsb_log_exp3
 
-sed -i 's/980/16/' config_exp3.yml
+sed -i 's/value_size: \[980\]/value_size: \[16\]/' config_exp3.yml
 echo "Experiment 3 done"
 
 echo "Running Experiment 4"
 sudo python3 run.py config_exp4.yml my_ycsb_log_exp4
-sed -i 's/0.01/0.02/' config_exp4.yml
+sed -i 's/optane_ratio: \[0.01\]/optane_ratio: \[0.02\]/' config_exp4.yml
 sudo python3 run.py config_exp4.yml my_ycsb_log_exp4
-sed -i 's/0.02/0.04/' config_exp4.yml
+sed -i 's/optane_ratio: \[0.02\]/optane_ratio: \[0.04\]/' config_exp4.yml
 sudo python3 run.py config_exp4.yml my_ycsb_log_exp4
-sed -i 's/0.04/0.08/' config_exp4.yml
+sed -i 's/optane_ratio: \[0.04\]/optane_ratio: \[0.08\]/' config_exp4.yml
 sudo python3 run.py config_exp4.yml my_ycsb_log_exp4
-sed -i 's/0.08/0.16/' config_exp4.yml
+sed -i 's/optane_ratio: \[0.08\]/optane_ratio: \[0.16\]/' config_exp4.yml
 sudo python3 run.py config_exp4.yml my_ycsb_log_exp4
 
-sed -i 's/0.16/0.01/' config_exp4.yml
+sed -i 's/optane_ratio: \[0.16\]/optane_ratio: \[0.01\]/' config_exp4.yml
 echo "Experiment 4 done"
